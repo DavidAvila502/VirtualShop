551a1b626f04f0a0b83057ed03f0ae5aa4efbcdb
David Avila <davidavila6002@gmail.com>
2024-11-02 21:32:53 -0600

    Added Root controller and NotFoundMiddleware

---
+++ VirtualShop/Infrastructure/Middlewares/Extensions/NotFoundMiddlewareExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace VirtualShop.Infrastructure.Middlewares.Extensions
+{
+    public static class NotFoundMiddlewareExtension
+    {
+        public static IApplicationBuilder UseNotFoundMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<NotFoundMiddleware>();
+        
+        }
+    }
+}
